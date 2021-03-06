19e6be29ce94d612566ed20031623d1a3477fd21
wgxshr <756278874@qq.com>
2021-03-06 16:23:35 +0800

    2021-03-06更新

--- mq-zipkin-barista-service/src/main/java/spring/springbucks/barista/repository/CoffeeOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package spring.springbucks.barista.repository;
-
-import spring.springbucks.barista.model.CoffeeOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoffeeOrderRepository extends JpaRepository<CoffeeOrder, Long> {
-}
--- sleuth-customer-service/src/main/java/spring/springbucks/customer/model/OrderState.java
+++
@@ -1,5 +0,0 @@
-package spring.springbucks.customer.model;
-
-public enum OrderState {
-    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
-}
